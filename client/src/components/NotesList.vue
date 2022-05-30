90e5ab30a9a48dc3dd32c5793810ff532b26a6e1
shotnothing <shotnothing@protonmail.ch>
2022-05-30 18:19:50 +0800

    search bar and list view

--- client/src/components/NotesList.vue
+++ client/src/components/NotesList.vue
@@ -1,10 +1,19 @@
 <template>
 
   <v-list two-line>
+
+    <v-text-field
+      label="Search Field (regex for now)"
+      hide-details="auto"
+      v-model="searchTerm" 
+    ></v-text-field>
+    <br>
+    <v-btn
+    @click.native="refreshNotesList()"
+    >Search</v-btn>
+
     <v-list-item-group
       v-model="selected"
-      active-class="pink--text"
-      multiple
     >
       <template v-for="(item, index) in items">
         <v-list-item :key="item.title">
@@ -17,11 +26,11 @@
                 v-text="item.headline"
               ></v-list-item-subtitle>
 
-              <v-list-item-subtitle v-text="item.subtitle"></v-list-item-subtitle>
+              <v-list-item-subtitle v-text="item.userId.username"></v-list-item-subtitle>
             </v-list-item-content>
 
             <v-list-item-action>
-              <v-list-item-action-text v-text="item.action"></v-list-item-action-text>
+              <v-list-item-action-text v-text="item.datePublished"></v-list-item-action-text>
 
               <v-icon
                 v-if="!active"
@@ -44,66 +53,60 @@
           v-if="index < items.length - 1"
           :key="index"
         ></v-divider>
+
       </template>
+
+    <br>
+    <v-btn
+    @click.native="viewNote()"
+    >View</v-btn>
+
     </v-list-item-group>
+
   </v-list>
 
 </template>
 
 <script>
-import VueSimplemde from 'vue-simplemde'
+import VueSimplemde from 'vue-simplemde';
+import { marked } from 'marked';
 export default {
   data() {
     return {
-      selected: [2],
-      items: [
-        {
-          action: '2 days ago',
-          headline: 'Notes and me',
-          subtitle: `Poo poo`,
-          title: 'Boon Keng',
-        },
-        {
-          action: '2 hrs ago',
-          headline: 'Me and notes',
-          subtitle: `Wish I could come, but I'm out of town this weekend.`,
-          title: 'User 2',
-        },
-        {
-          action: '6 hrs ago',
-          headline: 'Oui oui',
-          subtitle: 'Pee Pee',
-          title: 'User 3',
-        },
-        {
-          action: '12 hrs ago',
-          headline: 'Crossaint',
-          subtitle: 'Nteoarfaefasa',
-          title: 'User 4',
-        },
-        {
-          action: '18 hrs ago',
-          headline: 'Hello',
-          subtitle: 'We should eat this',
-          title: 'User 5',
-        },
-      ],
+      searchTerm: "STAR",
+      selected: [ ],
+      items: [ ],
+      selectedBody: "",
     };
   },
   components: {
     VueSimplemde,
   },
   methods: {
-    async uploadNote() {
+    async refreshNotesList() {
       let token = localStorage.getItem("jwt");
-      console.log(this.mde);
-      let response = await this.$http.post(
-        "/note/create",
-        this.mde,
-        { headers: { 'Authorization': token } }
+      let response = await this.$http.get(
+        "/note/search",
+        { body: {searchTerm: this.searchTerm},
+          headers: { 'Authorization': token } }
         );
-      console.log(response);
+      this.items = response.data.searchResults;
     },
+
+    async viewNote(){
+      let token = localStorage.getItem("jwt");
+      let response = await this.$http.get(
+        "/note/read",
+        { body: { 'noteId': this.items[this.selected]._id },
+          headers: { 'Authorization': token } }
+        );
+      this.selectedBody = response.data.searchResults;
+      console.log(this.items[this.selected]._id)
+      console.log(response);
+      console.log(this.selectedBody);
+
+      //return marked(this.selectedBody);
+    }
   }
 };
 </script>
