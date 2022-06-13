54aa84f7e2121bdf790dda0c526d521aee4939af
Hafeez <izzhafeez@gmail.com>
2022-06-13 19:40:49 +0800

    Edited colors on Login and Register Components

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -3,7 +3,7 @@
     <div class="row">
       <div class="col-lg-6 offset-lg-3 col-sm-10 offset-sm-1">
         <form
-          class="text-center border border-primary p-5"
+          class="text-center border border-secondary p-5"
           style="margin-top:70px;height:auto;padding-top:100px !important;"
           @submit.prevent="registerUser"
         >
@@ -47,13 +47,12 @@
           />
 
           <p>
-            Already have an account? <router-link to="/user/login"
-              >Login</router-link
-            >
+            Already have an account?
+            <router-link to="/user/login">Login</router-link>
 
             <!-- Sign in button -->
             <center>
-              <button class="btn btn-primary btn-block w-75 my-4" type="submit">
+              <button class="btn btn-secondary btn-block w-75 my-4" type="submit">
                 Sign Up
               </button>
             </center>
