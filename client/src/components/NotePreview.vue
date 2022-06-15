1d6d11a57cd56116458761e21d7e4d526e96a266
Hafeez <izzhafeez@gmail.com>
2022-06-15 17:54:45 +0800

    Added Note Preview object

---
+++ client/src/components/NotePreview.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="text-center">
+    <img src="@/assets/SampleThumbnail.png">
+    <div class="text-center">
+      <p>
+        {{ note.name }}
+        <br/>
+        <span class="text-secondary">{{ note.username }}</span>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NotePreview",
+  props: ["note"]
+};
+</script>
