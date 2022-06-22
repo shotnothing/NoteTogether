0e5a4dab52636a3a21c2622a7b4d17be3d5dfe12
Hafeez <izzhafeez@gmail.com>
2022-06-22 17:45:44 +0800

    Added View Window for discover mode

--- client/src/modules/discover/views/View.vue
+++ client/src/modules/discover/views/View.vue
@@ -1,38 +1,49 @@
 <template>
   <div class="container">
-    <div class="">
-      {{content}}
+    <div>
+      <RedditPreview :user="user" v-for="note in notes" :note="note" :key="note.votes"></RedditPreview>
     </div>
+    <div v-html="content" class="p-2"></div>
   </div>
 </template>
 
 <script>
+import RedditPreview from "@/components/RedditPreview.vue";
 import VueSimplemde from 'vue-simplemde';
 import { marked } from 'marked';
 
 export default {
   name: "DiscoverView",
-  props: ["user"],
+  props: ["user", "note"],
   components: {
+    RedditPreview,
   },
   data() {
     return {
       user: {},
       content: "",
+      notes: [],
     };
   },
   methods: {
     async viewNote(){
-      console.log(this.selected);
       try {
         let token = localStorage.getItem("jwt");
         let response = await this.$http.post(
           "/note/read",
           { noteId: this.$route.params.noteId },
           { headers: { 'Authorization': token } }
-          );
-        this.content = response.data.content.join();
-        // this.content = marked(response.data.content.join("\n"));
+        );
+        this.content = marked(response.data.content.join("<br>"));
+        // this.content = response;
+        this.notes = [{
+          title: response.data.title,
+          userId: {
+            username: response.data.noteCreator,
+          },
+          dateLastUpdated: response.data.dateLastUpdated,
+          votes: response.data.votes,
+        }];
       } catch (err) {
         // this.content = err;
       }
