d40a5b5be8579fde94b59e1bc733ce81b988c196
Hafeez <izzhafeez@gmail.com>
2022-06-22 23:02:44 +0800

    Deleted original markdown editor - Edit mode now uses one MDE

--- client/src/components/markdownEditor.vue
+++ client/src/components/markdownEditor.vue
@@ -1,42 +1,57 @@
 <template>
   <div>
-    <div class="input-group w-50">
-      <input
-        type="text"
-        label="Document Title"
-        class="form-control rounded"
-        placeholder="Insert Title Here"
-        v-model="title"/>
+    <div>
+      <h3>Edit Note</h3>
+      <span>Title</span>
+      <div class="input-group w-50">
+        <input
+          type="text"
+          id="title"
+          label="Document Title"
+          class="form-control rounded"
+          placeholder="Insert Title Here"
+          v-model="title"/>
+      </div>
+      <br>
+      <form>
+        <vue-simplemde 
+          v-model="content"
+          ref="noteUploadMDE"
+          v-if="loaded"
+        />
+      </form>
+
+      <div class="float-right w-50">
+        <button
+          class="float-right btn btn-secondary btn-block w-25 m-2"
+          type="button"
+          v-on:click="publishNote()"
+        >
+          Publish
+        </button>
+
+        <button
+          class="float-right btn btn-secondary btn-block w-25 m-2"
+          type="button"
+          v-on:click="saveNote()"
+        >
+          Save
+        </button>
+
+        <button
+          class="float-right btn btn-secondary btn-block w-25 m-2"
+          type="button"
+          v-on:click="renderNote()"
+        >
+          Render
+        </button>
+      </div>
+
+      <br><br>
+      <div 
+        v-html="renderedContent"
+      ></div>
     </div>
-    <br>
-    <vue-simplemde 
-      v-model="content" 
-      ref="noteUploadMDE"
-    />
-
-    <div class="float-right w-50">
-      <button
-        class="float-right btn btn-secondary btn-block w-25 m-2"
-        type="button"
-        v-on:click="uploadNote()"
-      >
-        Create
-      </button>
-
-      <button
-        class="float-right btn btn-secondary btn-block w-25 m-2"
-        type="button"
-        v-on:click="renderNote()"
-      >
-        Render
-      </button>
-    </div>
-
-    <br><br>
-    <div 
-      v-html="renderedContent"
-    ></div>
-
   </div>
 </template>
 
@@ -45,34 +60,34 @@ import VueSimplemde from 'vue-simplemde'
 import { marked } from 'marked';
 import swal from "sweetalert";
 export default {
-  name: "MarkdownEditor",
+  name: "MarkdownEditorView",
   data() {
     return {
-      title: "",
       content: "",
-        // placeholder: "## Welcome to NoteTogther! \n**Lorem ipsum** dolor _sit amet_, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. \n\n>  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. \n\n`Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.`\n\nExcepteur sint [occaecat](https://somelink) \n*  cupidatat non proident, \n*  sunt in culpa qui officia deserunt \n*  mollit anim id est laborum.\n\n![Poster](https://drive.google.com/drive/u/2/folders/1l86i2zFTW22skrEASi6uezud8QOXTRjO) <= embed links will be blocked to prevent cross-site scripting, will whitelist stuff on our servers in the future"
-      renderedContent: ""
+      title: "",
+      loaded: false,
+      renderedContent: "",
     };
   },
   components: {
     VueSimplemde,
   },
   methods: {
-    async uploadNote() {
+    async getNote() {
       try {
         let token = localStorage.getItem("jwt");
-        console.log(this.mde);
         let response = await this.$http.post(
-          "/note/create",
-          { title: this.title, content: this.content},
+          "/note/read",
+          { noteId: this.$route.params.noteId },
           { headers: { 'Authorization': token } }
-          );
-        swal("Success", "Upload Successful (But Not Published)", "success");
-        console.log(response);
+        );
+        this.content = response.data.content;
+        this.title = response.data.title;
+        this.loaded = true;
       } catch (err) {
         switch(err.request.status) {
-          case 409:
-            swal("Error", "You already have a document with the same name!", "error");
+          case 402:
+            this.$router.push("/purchase/"+this.$route.params.noteId);
             break;
           case 401:
             swal("Error", "Unauthorized or your session has expired! Please relog.", "error");
@@ -80,12 +95,72 @@ export default {
           default:
             swal("Error", 'Uhh, error {{err.request.status}}', "error");
         }
-        
+      }
+    },
+    async saveNote() {
+      try {
+        let token = localStorage.getItem("jwt");
+        console.log(this.mde);
+        if (this.title === "") {
+          swal("Error", "Please enter a title!", "error");
+          return;
+        }
+        if (this.content === "") {
+          swal("Error", "Please enter some content!", "error");
+          return;
+        }
+        let response = await this.$http.post(
+          "/note/update",
+          { title: this.title, content: this.content, noteId: this.$route.params.noteId },
+          { headers: { 'Authorization': token } }
+          );
+        swal("Success", "Save Successful", "success");
+        console.log(response);
+      } catch (err) {
+        switch(err.request.status) {
+          case 401:
+            swal("Error", "Unauthorized or your session has expired! Please relog.", "error");
+            break;
+          default:
+            swal("Error", 'Uhh, error {{err.request.status}}', "error");
+        }
+      }
+    },
+    async publishNote() {
+      try {
+        let token = localStorage.getItem("jwt");
+        console.log(this.mde);
+        if (this.title === "") {
+          swal("Error", "Please enter a title!", "error");
+          return;
+        }
+        if (this.content === "") {
+          swal("Error", "Please enter some content!", "error");
+          return;
+        }
+        let response = await this.$http.post(
+          "/note/publish",
+          { noteId: this.$route.params.noteId },
+          { headers: { 'Authorization': token } }
+          );
+        swal("Success", "Publish Successful", "success");
+        console.log(response);
+      } catch (err) {
+        switch(err.request.status) {
+          case 401:
+            swal("Error", "Unauthorized or your session has expired! Please relog.", "error");
+            break;
+          default:
+            swal("Error", 'Uhh, error {{err.request.status}}', "error");
+        }
       }
     },
     async renderNote() {
-      this.renderedContent = marked(this.content);
+      this.renderedContent = marked(this.mde.content);
     },
+  },
+  mounted() {
+    this.getNote();
   }
 };
 </script>
