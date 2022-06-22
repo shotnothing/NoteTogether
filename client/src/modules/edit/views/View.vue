67d3d1fd745807e7577c15ddbfecf2f3d11eb186
Hafeez <izzhafeez@gmail.com>
2022-06-22 17:52:07 +0800

    Edited View Page for Edit Mode

---
+++ client/src/modules/edit/views/View.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <MarkdownEditorView></MarkdownEditorView>
+  </div>
+</template>
+
+<script>
+import MarkdownEditorView from "@/components/MarkdownEditorView.vue";
+
+export default {
+  name: "CreateView",
+  components: {
+    MarkdownEditorView,
+  }
+}
+</script>
+
+<style scoped></style>
