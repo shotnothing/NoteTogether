9dc488bb2356fcd362f48ae44096751ab1639634
Hafeez <izzhafeez@gmail.com>
2022-06-22 17:54:42 +0800

    Changed the name of the markdown editor component

--- client/src/components/markdownEditor.vue
+++ client/src/components/markdownEditor.vue
@@ -1,23 +1,36 @@
 <template>
   <div>
+    <div class="input-group w-50">
+      <input
+        type="text"
+        label="Document Title"
+        class="form-control rounded"
+        placeholder="Insert Title Here"
+        v-model="title"/>
+    </div>
+    <br>
     <vue-simplemde 
-      v-model="mde.content" 
+      v-model="content" 
       ref="noteUploadMDE"
     />
-    <v-text-field
-      label="Document Title"
-      hide-details="auto"
-      v-model="mde.title" 
-    ></v-text-field>
-    <br>
 
-    <v-btn
-      @click.native="uploadNote()"
-    > Upload </v-btn>
+    <div class="float-right w-50">
+      <button
+        class="float-right btn btn-secondary btn-block w-25 m-2"
+        type="button"
+        v-on:click="uploadNote()"
+      >
+        Create
+      </button>
 
-    <v-btn
-      @click.native="renderNote()"
-    > Render </v-btn>
+      <button
+        class="float-right btn btn-secondary btn-block w-25 m-2"
+        type="button"
+        v-on:click="renderNote()"
+      >
+        Render
+      </button>
+    </div>
 
     <br><br>
     <div 
@@ -32,13 +45,13 @@ import VueSimplemde from 'vue-simplemde'
 import { marked } from 'marked';
 import swal from "sweetalert";
 export default {
+  name: "MarkdownEditor",
   data() {
     return {
-      mde: {
-        title: "Untitled Document",
-        content: "## Welcome to NoteTogther! \n**Lorem ipsum** dolor _sit amet_, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. \n\n>  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. \n\n`Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.`\n\nExcepteur sint [occaecat](https://somelink) \n*  cupidatat non proident, \n*  sunt in culpa qui officia deserunt \n*  mollit anim id est laborum.\n\n![Poster](https://drive.google.com/drive/u/2/folders/1l86i2zFTW22skrEASi6uezud8QOXTRjO) <= embed links will be blocked to prevent cross-site scripting, will whitelist stuff on our servers in the future"
-      },
-      renderedContent: "",
+      title: "",
+      content: "",
+        // placeholder: "## Welcome to NoteTogther! \n**Lorem ipsum** dolor _sit amet_, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. \n\n>  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. \n\n`Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.`\n\nExcepteur sint [occaecat](https://somelink) \n*  cupidatat non proident, \n*  sunt in culpa qui officia deserunt \n*  mollit anim id est laborum.\n\n![Poster](https://drive.google.com/drive/u/2/folders/1l86i2zFTW22skrEASi6uezud8QOXTRjO) <= embed links will be blocked to prevent cross-site scripting, will whitelist stuff on our servers in the future"
+      renderedContent: ""
     };
   },
   components: {
@@ -51,7 +64,7 @@ export default {
         console.log(this.mde);
         let response = await this.$http.post(
           "/note/create",
-          this.mde,
+          { title: this.title, content: this.content},
           { headers: { 'Authorization': token } }
           );
         swal("Success", "Upload Successful (But Not Published)", "success");
@@ -71,8 +84,12 @@ export default {
       }
     },
     async renderNote() {
-      this.renderedContent = marked(this.mde.content);
+      this.renderedContent = marked(this.content);
     },
   }
 };
 </script>
+
+<style scoped>
+  @import '~simplemde/dist/simplemde.min.css';
+</style>
