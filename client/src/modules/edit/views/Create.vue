f375d5338c83424f5ed20a389093e3f069619580
Hafeez <izzhafeez@gmail.com>
2022-06-22 17:48:36 +0800

    Edited View Page for Create Mode

---
+++ client/src/modules/edit/views/Create.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    <h3>Create New Note</h3>
+    <MarkdownEditor></MarkdownEditor>
+  </div>
+</template>
+
+<script>
+import MarkdownEditor from "@/components/MarkdownEditor.vue";
+
+export default {
+  name: "CreateView",
+  components: {
+    MarkdownEditor,
+  }
+}
+</script>
+
+<style scoped></style>
