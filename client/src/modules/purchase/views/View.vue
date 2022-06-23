54973f5f5cecb3e5d7cf7ff63c57312f4c52a3d7
Hafeez <izzhafeez@gmail.com>
2022-06-23 22:08:03 +0800

    Edited Purchase View - first additions

--- client/src/modules/purchase/views/View.vue
+++ client/src/modules/purchase/views/View.vue
@@ -1,13 +1,22 @@
 <template>
   <div class="container">
-    Hello
+    <Purchase :user="user"></Purchase>
   </div>
 </template>
 
 <script>
+import Purchase from "@/components/Purchase.vue";
+
 export default {
   name: "PurchaseView",
+  props: ["user"],
+  data() {
+    return {
+      user: {},
+    }
+  },
   components: {
+    Purchase,
   }
 }
 </script>
