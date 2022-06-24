b3f9dc178bf8ea1d7c1d88532a328365887655eb
Hafeez <izzhafeez@gmail.com>
2022-06-24 16:27:20 +0800

    Edited Purchase component - should theoretically work

--- client/src/components/Purchase.vue
+++ client/src/components/Purchase.vue
@@ -1,64 +1,124 @@
 <template>
-  <div class="m-2 bg-white d-flex">
-    <div class="text-center flex-fixed-width-item pt-2">
-      <a v-on:click="upvote()">
-        <span v-if="voteStatus=='upvote'" class="text-secondary">⬆</span>
-        <span v-else class="text-dark">⬆</span>
-      </a>
-      <br>
-      {{ votes }}
-      <br>
-      <a v-on:click="downvote()">
-        <span v-if="voteStatus=='downvote'" class="text-secondary">⬇</span>
-        <span v-else class="text-dark">⬇</span>
-      </a>
+  <div class="container d-flex">
+    <div class="flex-fill">
+      <RedditPreview v-for="note in notes" :note="note" :key="note"></RedditPreview>
+      <h3>Preview:</h3>
+      <div class="m-2 p-2 bg-white border border-secondary">{{ preview }}</div>
     </div>
-    <div class="text-truncate py-2">
-      <button class="lead">
-        <a v-bind:href="'/discover/'+note._id" class="text-dark">{{ note.title }}</a>
-        <span v-if="isLocked" class="ml-2">🔒</span>
-        <a v-else class="ml-2" v-on:click="favourite()">
-          <span v-if="isFavourited" class="text-secondary">★</span>
-          <span v-else class="text-dark">☆</span>
-        </a>
-      </button>
-      <div class="font-weight-light small">{{ note.userId.username }}</div>
-      <div class="font-weight-light small">{{ this.timeDiff }}</div>
+    <div class="m-2">
+      <CreditBalance :user="user"></CreditBalance>
+      <div class="w-100">Create notes or write reviews to earn credits!</div>
     </div>
-    <div class="ml-auto p-2">
-      <div v-if="note.votes>100" class="color-gold">
-        Gold Tier  
-      </div>
-      <div v-else-if="note.votes>50" class="color-silver">
-        Silver Tier
-      </div>
-      <div v-else-if="note.votes>20" class="color-bronze">
-        Bronze Tier
-      </div>
-      <div v-else>
-        Free Tier
-      </div>
-      <br>
-    </div>
+    <button
+      class="float-right btn btn-secondary btn-block w-25 m-2"
+      type="button"
+      v-on:click="purchaseNote()"
+    >
+      Purchase
+    </button>
   </div>
 </template>
 
 <script>
+import CreditBalance from "@/components/CreditBalance";
+import RedditPreview from "@/components/RedditPreview";
+
 export default {
-  name: "NotePreview",
-  props: ["note", "user"],
+  name: "Purchase",
+  props: ["user"],
   data() {
     return {
-      timeDiff: "invalid",
-      voteStatus: "clear",
-      isLocked: false,
-      isFavourited: false,
-      votes: 0,
+      tier: "",
+      price: 0,
+      data: {},
+      notes: [],
+      preview: "",
     }
   },
-  methods: {
+  components: {
+    CreditBalance,
+    RedditPreview
   },
-  created() {
+  methods: {
+    async getTier() {
+      try {
+        let token = localStorage.getItem("jwt");
+        let response = await this.$http.post(
+          "/note/getTier",
+          { noteId: this.$route.params.noteId },
+          { headers: { 'Authorization': token } }
+        );
+        this.tier = response.data.tier;
+        this.price = response.data.price;
+      } catch (err) {
+        swal("Error", err.response, "error");
+      }
+    },
+    async viewNote(){
+      try {
+        let token = localStorage.getItem("jwt");
+        let response = await this.$http.post(
+          "/note/read",
+          { noteId: this.$route.params.noteId },
+          { headers: { 'Authorization': token } }
+        );
+        // swal("Success", "Note has already been purchased or is free", "success");
+        this.$router.push("/discover/"+this.$route.params.noteId);
+      } catch (err) {
+        switch (err.response.status) {
+          case 402:
+            this.preview = response.data.preview;
+            this.notes = [{
+              title: response.data.title,
+              userId: {
+                username: response.data.username,
+              },
+              dateLastUpdated: response.data.dateLastUpdated,
+              votes: response.data.votes,
+              _id: this.$route.params.noteId
+            }];
+            break;
+          default:
+            swal("Error", err.response, "error");
+        }
+        swal("Error", err, "error");
+      }
+    },
+    async purchaseNote() {
+      try {
+        let token = localStorage.getItem("jwt");
+        console.log(this.mde.content);
+        let response = await this.$http.post(
+          "/note/purchase",
+          { noteId: this.$route.params.noteId },
+          { headers: { 'Authorization': token } }
+        );
+        swal("Success", "Purchase Successful", "success");
+        let favouriteResponse = await this.$http.post(
+          "/note/favourite",
+          { noteId: this.$route.params.noteId, action: "favourite" },
+          { headers: { 'Authorization': token } }
+        );
+        this.$router.push("/study/"+this.$route.params.noteId);
+        console.log(response);
+        console.log(favouriteResponse);
+      } catch (err) {
+        switch(err.request.status) {
+          case 405:
+            swal("Error", "Not enough credits to purchase this", "error");
+            break;
+          case 401:
+            swal("Error", "Unauthorized or your session has expired! Please relog.", "error");
+            break;
+          default:
+            swal("Error", `Uhh, error ${err.request.status}`, "error");
+        }
+      }
+    }
+  },
+  async created() {
+    await this.getTier();
+    await this.viewNote();
   }
 };
 </script>
