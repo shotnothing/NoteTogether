c1ae1d325a9fe569e48a70ea458d99ede57ea32f
shotnothing <shotnothing@protonmail.ch>
2022-06-27 02:28:22 +0800

    staging to heroku

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,6 +1,11 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  devServer: { allowedHosts: "all", },
   transpileDependencies: [
     'vuetify'
   ]
 })
+
+{
+  
+};
