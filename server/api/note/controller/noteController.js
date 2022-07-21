022fb31297c9871a646c1282dae387def8b4599c
Hafeez <izzhafeez@gmail.com>
2022-07-22 01:11:03 +0800

    Edited Note Controller

--- server/api/note/controller/noteController.js
+++ server/api/note/controller/noteController.js
@@ -1,42 +1,43 @@
 const Note = require("../model/Note");
 const User = require("../../user/model/User");
+const Review = require("../../review/model/Review");
 
 const purchaseController = require("./purchaseController");
+const { rewriteDefault } = require("@vue/compiler-sfc");
 
-const PER_PAGE = 5;
+const PER_PAGE = 10;
 const PREVIEW_LEN = 5;
-const MAX_TITLE_LENGTH = 32;
+const MAX_TITLE_LENGTH = 50;
+const CREDITS_AWARDED_AUTHOR_PUBLISH = 1;
+
+const GOLD_TIER = 200;
+const SILVER_TIER = 100;
+const BRONZE_TIER = 50;
+
+const GOLD_PRICE = 4;
+const SILVER_PRICE = 2;
+const BRONZE_PRICE = 1;
 
 exports.createNote = async (req, res) => {
   try {
     // Id of requestor
     const userId = req.userData._id;
-    
-    // Checks whether note of the same title has been created by the user
-    const noteSearch = await Note.find({
-      title: req.body.title,
-      userId: userId,
-      isDeleted: false
-    });
-
-    // User is not allowed to create two notes of the same name
-    // if (noteSearch.length >= 1) {
-    //   return res.status(409).json({
-    //     note: noteSearch[0],
-    //     err: "You have already created a note with this name!"
-    //   });
-    // }
 
     // check title length
     if (req.body.title.length > MAX_TITLE_LENGTH) {
-      return res.status(401).json({ err: "Title is too long" });
+      return res.status(406).json({ err: "Title is too long" });
     }
+
+    const content = req.body.content.split('\n');
+    const isCheatsheet = req.body.isCheatsheet || false;
 
     // Create note (assume first that its not a fork)
     var note = new Note({
       title: req.body.title,
-      content: req.body.content.split('\n'),
-      userId: userId
+      content: content,
+      length: content.length,
+      userId: userId,
+      isCheatsheet: isCheatsheet
     });
 
     if (!!req.body.forkOf) {
@@ -60,17 +61,24 @@ exports.createNote = async (req, res) => {
         title: req.body.title,
         content: out,
         userId: userId,
-        forkOf: req.body.forkOf
+        length: req.body.content.length,
+        forkOf: req.body.forkOf,
+        isCheatsheet: isCheatsheet
       });
     }
 
     // Adds new note to user's collection
     let savedNote = await note.save();
     let user = await User.findById(userId);
-    user.notes = [note._id, ...user.notes];
+    // user.notes.push(savedNote._id);
+    const status = await User.findByIdAndUpdate(userId, {
+      $addToSet: {
+        notes: savedNote._id, 
+      } 
+    });
     await user.save();
 
-    res.status(200).json({ note: savedNote });
+    res.status(201).json({ note: savedNote });
   } catch (err) {
     console.log(err);
     res.status(400).json({ err: err });
@@ -85,50 +93,61 @@ exports.readNote = async (req, res) => {
 
     // Id of the requestor
     const userId = req.userData._id;
-
-    // Id of the creator of the note
-    const authorId = note.userId;
     let user = await User.findById(userId);
-    let author = await User.findById(authorId);
-    const username = author.username;
+
+    // Id of the author
+    const authorId = note.userId;
 
     // Can access the note if you created it, or if it's published
-    if (!note.isPublished && authorId != userId) {
+    if ((!note.isPublished || note.isDeleted) && authorId != userId) {
       return res.status(401).json({ err: "Not authorised/published" });
     }
 
-    let content = await resolveFork(note)
+    let content = await resolveFork(note);
 
-    // Must purchase note if
-    if (purchaseController.getTier(note) != 'none'  // its not free 
-      && authorId != userId                         // you dont own the note
-      && !user.purchased.includes(noteId)           // you have not purchased it before
-      && content.length > PREVIEW_LEN               // no need to pay for short notes
-    ) {
-      return res.status(402).json({ 
-        err: "Need to purchase",
-        preview: content.slice(0, PREVIEW_LEN).join(),
-        title: note.title,
-        username: username,
-        dateLastUpdated: note.dateLastUpdated,
-        dateCreated: note.dateCreated,
-        votes: note.votes,
-        noteId: noteId
+    let baseNoteInformation = await getBaseNoteInformation(note);
+    let additionalInformation = await getAdditionalInformation(note, user);
+    baseNoteInformation["isFavourited"] = additionalInformation.isFavourited;
+    baseNoteInformation["isLocked"] = additionalInformation.isLocked;
+    baseNoteInformation["voteStatus"] = additionalInformation.voteStatus;
+
+    baseNoteInformation["reviews"] = [];
+    for (reviewId of note.reviews.reverse()) {
+      let review = await Review.findById(reviewId.toString())
+        .populate("userId", "username")
+        .select("title content username dateCreated votes")
+        .lean();
+      review["voteStatus"] = await getVoteInformation(review, user);
+      baseNoteInformation.reviews.push(review);
+    }
+
+    // Must purchase note if locked
+    if (additionalInformation.isLocked) {
+      baseNoteInformation["err"] = "Need to purchase";
+      baseNoteInformation["preview"] = content.slice(0, PREVIEW_LEN).join("\n");
+      for ([key, val] of Object.entries(additionalInformation)) {
+        baseNoteInformation[key] = val;
+      }
+      return res.status(402).json(baseNoteInformation);
+    }
+
+    baseNoteInformation["content"] = content.join("\n");
+
+    if (!user.purchased.includes(noteId)) {
+      // user.purchased.push(noteId);
+      const status = await User.findByIdAndUpdate(userId, {
+        $addToSet: {
+          purchased: noteId, 
+        } 
       });
     }
 
-    res.status(200).json({
-      title: note.title,
-      content: (await resolveFork(note)).join(),
-      raw_content: note.content,
-      username: username,
-      dateLastUpdated: note.dateLastUpdated,
-      dateCreated: note.dateCreated,
-      votes: note.votes,
-      noteId: noteId,
-      isPublished: note.isPublished,
-      isDeleted: note.isDeleted,
-    });
+    const userStatus = await user.save();
+    for ([key, val] of Object.entries(additionalInformation)) {
+      baseNoteInformation[key] = val;
+    }
+
+    return res.status(200).json(baseNoteInformation);
   } catch (err) {
     console.log(err);
     res.status(400).json({ err: err });
@@ -166,6 +185,7 @@ exports.updateNote = async (req, res) => {
     } else {
       note.content = req.body.content;
     }
+    note.length = req.body.content.length;
     note.title = req.body.title;
     note.dateLastUpdated = Date.now();
     await note.save();
@@ -244,18 +264,35 @@ exports.publishNote = async (req, res) => {
     await note.save();
 
     // Create new private note that can still be edited
+    // The private note will increment the version number of the note
+    let newTitle = "";
+    const lastLetter = note.title.charAt(note.title.length-1);
+    const secondLastLetter = note.title.charAt(note.title.length-2);
+    if (secondLastLetter == "V") {
+      newTitle = note.title.slice(0, -1) + (parseInt(lastLetter) + 1).toString();
+    } else {
+      newTitle = note.title + " V2";
+    }
     const newPrivateNote = new Note({
-      title: note.title,
+      title: newTitle,
       userId: noteCreatorId,
       content: note.content,
     });
 
     // Adds new note to user's collection
     const savedNote = await newPrivateNote.save();
-    user.notes = [newPrivateNote._id, ...user.notes];
+    // user.notes.push(newPrivateNote._id);
+    const status = await User.findByIdAndUpdate(userId, {
+      $inc: {
+        credits: CREDITS_AWARDED_AUTHOR_PUBLISH, 
+      },
+      $addToSet: {
+        notes: newPrivateNote._id,
+      }
+    });
     await user.save();
 
-    res.status(200).json({ note: savedNote });
+    res.status(201).json({ note: savedNote });
   } catch (err) {
     res.status(400).json({ err: err });
   }
@@ -263,6 +300,10 @@ exports.publishNote = async (req, res) => {
 
 exports.searchNote = async (req, res) => {
   try {
+    // Id of requestor
+    const userId = req.userData._id;
+    let user = await User.findById(userId);
+
     const notes = await Note
       .find({
         title: { $regex: new RegExp(req.body.searchTerm, "i") },
@@ -271,14 +312,104 @@ exports.searchNote = async (req, res) => {
       })
       .sort({ votes: -1 })
       .skip(PER_PAGE*(req.body.page-1))
-      .limit(PER_PAGE)
+      .limit(PER_PAGE+1)
       .populate("userId", "username")
-      .select("_id title userId dateLastUpdated username votes favourites");
+      .select("_id title userId dateLastUpdated username votes favourites reviewCount forkOf length isCheatsheet")
+      .lean();
+    
+    const hasNextPage = notes.length > PER_PAGE;
+    if (hasNextPage) {
+      notes.pop();
+    };
+    const hasPreviousPage = req.body.page > 1;
+    
+    let newNotes = [];
+    for (let note of notes) {
+      const tierPrice = getTier(note);
+      let newNote = note;
+      const additionalInformation = await getAdditionalInformation(note, user);
+      newNote["tier"] = tierPrice.tier;
+      newNote["price"] = tierPrice.price;
+      newNote["username"] = await getUsernameChain(note);
+      newNote["isFavourited"] = additionalInformation.isFavourited;
+      newNote["isLocked"] = additionalInformation.isLocked;
+      newNote["voteStatus"] = additionalInformation.voteStatus;
+      newNotes.push(newNote);
+    }
 
-    res.status(200).json({ searchResults: notes });
+    res.status(200).json({
+      hasPreviousPage: hasPreviousPage,
+      hasNextPage: hasNextPage,
+      searchResults: newNotes
+    });
   } catch (err) {
+    console.log(err);
     res.status(400).json({ err: err });
   }
+}
+
+async function isLocked(noteId, userId) {
+  const note = await Note.findById(noteId);
+  const user = await User.findById(userId);
+  const tierPrice = getTier(note);
+  const tier = tierPrice.tier;
+  // The note is locked if
+  return tier != "free"                      // its not free 
+    && note.userId != userId                      // you dont own the note
+    && !user.purchased.includes(noteId)           // you have not purchased it before
+    && note.content.length > PREVIEW_LEN;         // the note is short enough
+}
+
+async function getBaseNoteInformation(note) {
+  const tierPrice = getTier(note);
+  const toReturn = {
+    noteId: note._id,
+    title: note.title,
+    dateLastUpdated: note.dateLastUpdated,
+    dateCreated: note.dateCreated,
+    votes: note.votes,
+    favourites: note.favourites,
+    tier: tierPrice.tier,
+    price: tierPrice.price,
+    reviewCount: note.reviewCount,
+    isCheatsheet: note.isCheatsheet
+  };
+  toReturn["username"] = await getUsernameChain(note);
+
+  return toReturn;
+}
+
+async function getUsernameChain(note) {
+  let currUser = await User.findById(note.userId);
+  let username = currUser.username;
+  let currNote = note;
+  while (!!currNote.forkOf) {
+    currNote = await Note.findById(currNote.forkOf);
+    currUser = await User.findById(currNote.userId);
+    username += ", " + currUser.username;
+  }
+  return username;
+}
+
+async function getAdditionalInformation(note, user) {
+  let additionalInformation = {};
+  const noteId = note._id.toString();
+  additionalInformation["isFavourited"] = user.favourited.includes(note._id);
+  additionalInformation["isLocked"] = await isLocked(noteId, user._id);
+  additionalInformation["voteStatus"] = await getVoteInformation(note, user);
+
+  return additionalInformation;
+}
+
+async function getVoteInformation(item, user) {
+  let voteStatus = "no vote"
+  for (let vote of user.voted) {
+    if (vote.id == item._id) {
+      voteStatus = vote.isUpvoted ? "upvote" : "downvote";
+      break;
+    }
+  }
+  return voteStatus;
 }
 
 function myers(o, n) {
@@ -336,7 +467,7 @@ function myers(o, n) {
   for (var i in n) {
       if (!Array.isArray(n[i])) {
           diff.push('+' + i)
-          diff.push(n[i])
+          diff.push(n[i].slice(0, n.length - 1))
       }
   }
   
@@ -355,7 +486,7 @@ function unmyers(n, diff) {
             o.splice(addr + shift, 1)
             shift -= 1;
         } else if (op == '+') {
-            o.splice(addr, 0, diff[i + 1])
+            o.splice(addr, 0, diff[i + 1]) + "\n"
         }
     }
     
@@ -368,4 +499,29 @@ async function resolveFork(note) {
     return unmyers(await resolveFork(parent), note.content)
   }
   return note.content;
+}
+
+function getTier(note) {
+  const metric = note.votes + (2*note.favourites);
+  if (metric < BRONZE_TIER || note.length < PREVIEW_LEN) {
+    return {
+      tier: "free",
+      price: 0
+    };
+  } else if (metric < SILVER_TIER) {
+    return {
+      tier: "bronze",
+      price: BRONZE_PRICE
+    };
+  } else if (metric < GOLD_TIER) {
+    return {
+      tier: "silver",
+      price: SILVER_PRICE
+    };
+  } else {
+    return {
+      tier: "gold",
+      price: GOLD_PRICE
+    };
+  }
 }
