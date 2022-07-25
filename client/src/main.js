3cda6a25184ead5016ae03f7dbe6dfb6b5df1660
shotnothing <shotnothing@protonmail.ch>
2022-07-25 18:09:30 +0800

    refactored and removed debug stubs. also who is juan and edgard?

--- client/src/main.js
+++ client/src/main.js
@@ -6,8 +6,8 @@ import "./styles/_custom.scss";
 import vuetify from './plugins/vuetify'
 
 const base = axios.create({
-  baseURL: "https://notetogether-server.herokuapp.com"
-  //baseURL: "http://localhost:4000"
+  //baseURL: "https://notetogether-server.herokuapp.com"
+  baseURL: "http://localhost:4000"
 });
 
 Vue.prototype.$http = base;
