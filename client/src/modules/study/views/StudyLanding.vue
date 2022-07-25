d7f15652c91388cbfd02a5a24e95d3b3b79f815d
Hafeez <izzhafeez@gmail.com>
2022-07-25 18:49:36 +0800

    Edited Study and Edit Landing pages - renamed private to unpublished notes

--- client/src/modules/study/views/StudyLanding.vue
+++ client/src/modules/study/views/StudyLanding.vue
@@ -8,7 +8,7 @@
         Nothing to show here! 
         <br><br></div>
     <br>
-    <h3>Private Notes</h3>
+    <h3>Unpublished Notes</h3>
     <div class=row>
       <NotePreview class="col-sm-6 col-md-2" v-for="note in unpublishedNotes" :note="note" :key="note" :address="address" :userId="userId"></NotePreview>
     </div>
